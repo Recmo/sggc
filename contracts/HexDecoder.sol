d37e7d4d915cf070dfd9256da7850930808b8f4e
Remco Bloemen <remco@wicked.ventures>
2018-05-26 14:43:33 +0200

    Hex 306850: manual loop

--- contracts/HexDecoder.sol
+++ contracts/HexDecoder.sol
@@ -4,43 +4,46 @@ contract HexDecoder {
     
     function () payable { assembly {
         
-        let i := 68
         let o := 64
+        let i := 68
+        let a
         
-        for {} lt(i, calldatasize) {} {
-            
-            let a := calldataload(i)
-            
-            // Convert characters to nibbles
-            a := add(and(a,
+        jumpi(empty, eq(i, calldatasize))
+        
+    loop:
+        a := calldataload(i)
+        
+        // Convert characters to nibbles
+        a := add(and(a,
 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F
-            ), mul(9, and(div(a, 64),
+        ), mul(9, and(div(a, 64),
 0x0101010101010101010101010101010101010101010101010101010101010101
-            )))
-            
-            // Shuffle odd nibbles to consecutive bytes
-            a := and(div(mul(a, 0x11), 0x10),
-0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF
-            )
-            a := and(div(mul(a, 0x101), 0x100),
-0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF
-            )
-            a := and(div(mul(a, 0x10001), 0x10000),
-0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF
-            )
-            a := and(div(mul(a, 0x100000001), 0x100000000),
-0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF
-            )
-            a := mul(a, 0x100000000000000010000000000000000)
-            
-            // Store
-            mstore(o, a)
-            o := add(o, 16)
-            
-            // Loop
-            i := add(i, 32)
-        } 
+        )))
         
+        // Shuffle odd nibbles to consecutive bytes
+        a := and(div(mul(a, 0x11), 0x10),
+0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF
+        )
+        a := and(div(mul(a, 0x101), 0x100),
+0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF
+        )
+        a := and(div(mul(a, 0x10001), 0x10000),
+0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF
+        )
+        a := and(div(mul(a, 0x100000001), 0x100000000),
+0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF
+        )
+        a := mul(a, 0x100000000000000010000000000000000)
+        
+        // Store
+        mstore(o, a)
+        o := add(o, 16)
+        
+        // Loop
+        i := add(i, 32)
+        jumpi(loop, lt(i, calldatasize))
+        
+    exit:
         let ol := div(calldataload(36), 2)
         mstore(0, 32)
         mstore(32, ol)
@@ -49,5 +52,9 @@ contract HexDecoder {
         return(0, and(add(ol, 95),
             0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0
         ))
+        
+    empty:
+        mstore(0, 32)
+        return(0, 64)
     }}
 }
