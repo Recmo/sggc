e3ebaad4a074c3ac9013ad5b9948c27ba1b04d33
Remco Bloemen <remco@wicked.ventures>
2018-05-26 13:09:13 +0200

    Hex: use calldata

--- contracts/HexDecoder.sol
+++ contracts/HexDecoder.sol
@@ -7,52 +7,34 @@
 
 pragma solidity 0.4.24;
 
-// 0  0011 0000 -> 0000
-// 1  0011 0001 -> 0001
-// 2  0011 0010 -> 0010
-// 3  0011 0011 -> 0011
-// 4  0011 0100 -> 0100
-// 5  0011 0101 -> 0101
-// 6  0011 0110 -> 0110
-// 7  0011 0111 -> 0111
-// 8  0011 1000 -> 1000
-// 9  0011 1001 -> 1001
-// a  0110 0001 -> 1010
-// b  0110 0010 -> 1011
-// c  0110 0011 -> 1100
-// d  0110 0100 -> 1101
-// e  0110 0101 -> 1110
-// f  0110 0110 -> 1111
-// A  0100 0001 -> 1010
-// B  0100 0010 -> 1011
-// C  0100 0011 -> 1100
-// D  0100 0100 -> 1101
-// E  0100 0101 -> 1110
-// F  0100 0110 -> 1111
-
 contract HexDecoder {
 
-    /**
-     * @dev Decodes a hex-encoded input string, returning it in binary.
-     *
-     * Input strings may be of any length, but will always be a multiple of two
-     * bytes long, and will not contain any non-hexadecimal characters.
-     *
-     * @param input The hex-encoded input.
-     * @return The decoded output.
-     */
-    function decode(string input)
-        public pure
-        returns(bytes output)
+    //function () payable
+    function decode(string) external payable returns(bytes)
     {
-        uint256 ol = bytes(input).length / 2;
-        output = new bytes(ol);
+        uint256 ol;
+        assembly {
+            ol := div(calldataload(36), 2)
+        }
+        bytes memory output = new bytes(ol);
         for(uint i = 0; i < ol; i++) {
-            uint8 a = uint8(bytes(input)[i * 2]);
-            uint8 b = uint8(bytes(input)[i * 2 + 1]);
+            
+            uint8 a;
+            uint8 b;
+            assembly {
+                let iaddr := add(38, mul(i, 2))
+                let ab := calldataload(iaddr)
+                a := and(div(ab, 0x100), 0xFF)
+                b := and(ab, 0xFF)
+            }
+            
             a = (a & 0xf) + ((a / 64) * 9);
             b = (b & 0xf) + ((b / 64) * 9);
             output[i] = byte((a << 4) | b);
+        }
+        assembly {
+            mstore(sub(output, 32), 32)
+            return(sub(output, 32), add(ol, 64))
         }
     }
 }
