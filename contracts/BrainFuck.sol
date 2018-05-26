a87dc16f3d59d42e46f48df91cac566102aa62b5
Remco Bloemen <remco@wicked.ventures>
2018-05-26 19:48:35 +0200

    Brain: virtual machine and compiler

--- contracts/BrainFuck.sol
+++ contracts/BrainFuck.sol
@@ -1,86 +1,128 @@
-/**
- * This file is part of the 1st Solidity Gas Golfing Contest.
- *
- * This work is licensed under Creative Commons Attribution ShareAlike 3.0.
- * https://creativecommons.org/licenses/by-sa/3.0/
- */
-
 pragma solidity 0.4.24;
 
 contract BrainFuck {
-    /**
-     * @dev Executes a BrainFuck program, as described at https://en.wikipedia.org/wiki/Brainfuck.
-     *
-     * Memory cells, input, and output values are all expected to be 8 bit
-     * integers. Incrementing past 255 should overflow to 0, and decrementing
-     * below 0 should overflow to 255.
-     *
-     * Programs and input streams may be of any length. The memory tape starts
-     * at cell 0, and will never be moved below 0 or above 1023. No program will
-     * output more than 1024 values.
-     *
-     * @param program The BrainFuck program.
-     * @param input The program's input stream.
-     * @return The program's output stream. Should be exactly the length of the
-     *          number of outputs produced by the program.
-     */
+    
     function execute(bytes program, bytes input)
         public view
         returns(bytes)
     {
-        uint ipp = 0;
-        uint opp = 0;
-        uint dp = 0;
-        bytes memory mem = new bytes(1024);
-        bytes memory output = new bytes(1024);
+        uint256[10] memory inst;
+        uint256[] memory prog;
         
-        
-        uint256 temp;
         assembly {
-        
-        // DIRECT THREADING !!
-        
-        test_label:
-            temp := test_label
-            temp := add(temp, 4)
-            jump(temp)
+            //
+            // BrainFuck virtual machine in the Ethereum virtual machine
+            //
             
-        go_left:
-            jump(next_instruction)
-        
-        go_right:
-            jump(next_instruction)
-        
-        increment:
-            jump(next_instruction)
-        
-        input:
-            jump(next_instruction)
+            // Tape is allocated 32...1055 in memory as bytes (use mstore8)
+            // Output is allocatd 1056...2079 in memory as bytes (use mstore8)
+            // Program is stored as 2080... as uint256 in direct threading
+            // Input is kept in calldata. Input pointer is offset by -31 so
+            // a byte can be read using and(calldataload(ip), 0xff)
             
+            let tp // Tape pointer
+            let ip // Input pointer
+            let op // Output pointer
+            let pp // Program pointer
+            
+            prog := sub(2080, 32)
+            mstore(prog, 1024)
+            mstore(2080, export)
+            
+        reset: // 0
+            tp := 32
+            ip := add(calldataload(36), 5) // offset left 31 bytes
+            op := 1056
+            pp := 2080
+            jump(mload(pp))
+            
+        right: // 1
+            tp := add(tp, 1)
+            pp := add(pp, 32)
+            jump(mload(pp))
+        
+        left: // 2
+            tp := sub(tp, 1)
+            pp := add(pp, 32)
+            jump(mload(pp))
+        
+        incr: // 3
+            mstore8(tp, add(mload(sub(tp, 31)), 1))
+            pp := add(pp, 32)
+            jump(mload(pp))
+        
+        decr: // 4
+            mstore8(tp, sub(mload(sub(tp, 31)), 1))
+            pp := add(pp, 32)
+            jump(mload(pp))
+        
+        output: // 5
+            mstore8(op, mload(sub(tp, 31)))
+            op := add(op, 1)
+            pp := add(pp, 32)
+            jump(mload(pp))
+        
+        input: // 6
+            mstore8(tp, calldataload(ip))
+            ip := add(ip, 1)
+            pp := add(pp, 32)
+            jump(mload(pp))
+        
+        open: // 7
+            jumpi(take, iszero(and(mload(sub(tp, 31)), 0xff))) 
+            pp := add(pp, 64)
+            jump(mload(pp))
+        
+        close: // 8
+            jumpi(take, and(mload(sub(tp, 31)), 0xff)) 
+            pp := add(pp, 64)
+            jump(mload(pp))
+        
+        take:
+            pp := mload(add(pp, 32))
+            jump(mload(pp))
+        
+        exit: // 9
+            mstore(992, 32)
+            mstore(1024, sub(op, 1056))
+            return(992, and(sub(op, 961), not(0x1F)))
+            
+        export:
+            mstore(inst, reset)
+            mstore(add(inst, 0x20), right)
+            mstore(add(inst, 0x40), left)
+            mstore(add(inst, 0x60), incr)
+            mstore(add(inst, 0x80), decr)
+            mstore(add(inst, 0xA0), output)
+            mstore(add(inst, 0xC0), input)
+            mstore(add(inst, 0xE0), open)
+            mstore(add(inst, 0x100), close)
+            mstore(add(inst, 0x120), exit)
         }
         
-        temp += 2;
-        
-        
-
+        uint256 progp = 0;
         for(uint ip = 0; ip < program.length; ip++) {
             byte instruction = program[ip];
             if(instruction == '+') {
-                mem[dp] = byte(uint(mem[dp]) + 1);
+                prog[progp++] = inst[3];
             } else if(instruction == '-') {
-                mem[dp] = byte(uint(mem[dp]) - 1);
+                prog[progp++] = inst[4];
             } else if(instruction == '>') {
-                dp++;
+                prog[progp++] = inst[1];
             } else if(instruction == '<') {
-                dp--;
+                prog[progp++] = inst[2];
             } else if(instruction == '.') {
-                output[opp++] = mem[dp];
+                prog[progp++] = inst[5];
             } else if(instruction == ',') {
-                mem[dp] = input[ipp++];
+                prog[progp++] = inst[6];
             } else if(instruction == '[') {
+                prog[progp++] = inst[7];
+                prog[progp++] = inst[9];
+                
+                /*
                 if(mem[dp] == 0) {
                     uint depth = 1;
-                    for(uint i = ip + 1; i < program.length; i++) {
+                    for(uint i = ip + 1; i < pl; i++) {
                         if(program[i] == ']') {
                             depth--;
                             if(depth == 0) {
@@ -92,7 +134,12 @@ contract BrainFuck {
                         }
                     }
                 }
+                */
             } else if(instruction == ']') {
+                prog[progp++] = inst[8];
+                prog[progp++] = inst[9];
+                
+                /*
                 if(mem[dp] != 0) {
                     depth = 1;
                     for(i = ip - 1; i > 0; i--) {
@@ -107,13 +154,13 @@ contract BrainFuck {
                         }
                     }
                 }
+                */
             }
         }
-
-        bytes memory ret = new bytes(opp);
-        for(i = 0; i < opp; i++) {
-            ret[i] = output[i];
+        prog[progp++] = inst[9];
+        
+        assembly {
+            jump(mload(inst)) // reset
         }
-        return ret;
     }
 }
