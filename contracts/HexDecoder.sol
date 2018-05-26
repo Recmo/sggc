28a896270a28d1a7ac1e5305afd861bdfe77cc92
Remco Bloemen <remco@wicked.ventures>
2018-05-26 14:39:44 +0200

    Hex 308276: asm loop, fixed mem

--- contracts/HexDecoder.sol
+++ contracts/HexDecoder.sol
@@ -2,56 +2,52 @@ pragma solidity ^0.4.23;
 
 contract HexDecoder {
     
-    function () payable {
+    function () payable { assembly {
         
-        uint256 ol;
-        assembly {
-            ol := div(calldataload(36), 2)
-        }
-        bytes memory output = new bytes(ol);
+        let i := 68
+        let o := 64
         
-        for(uint i = 0; i < ol; i += 16) {
+        for {} lt(i, calldatasize) {} {
             
-            uint256 a;
-            assembly {
-                
-                let iaddr := add(mul(i, 2), 68)
-                a := calldataload(iaddr)
-                
-                // Convert characters to nibbles
-                a := add(and(a,
+            let a := calldataload(i)
+            
+            // Convert characters to nibbles
+            a := add(and(a,
 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F
-                ), mul(9, and(div(a, 64),
+            ), mul(9, and(div(a, 64),
 0x0101010101010101010101010101010101010101010101010101010101010101
-                )))
-                
-                // Shuffle odd nibbles to consecutive bytes
-                a := and(div(mul(a, 0x11), 0x10),
-0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF
-                )
-                a := and(div(mul(a, 0x101), 0x100),
-0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF
-                )
-                a := and(div(mul(a, 0x10001), 0x10000),
-0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF
-                )
-                a := and(div(mul(a, 0x100000001), 0x100000000),
-0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF
-                )
-                a := mul(a, 0x100000000000000010000000000000000)
-                
-                // Store
-                let oaddr := add(i, add(output, 32))
-                mstore(oaddr, a)
-            }
-        }
-        assembly {
-            mstore(sub(output, 32), 32)
+            )))
             
-            // Add 64 to ol and round to the next multiple of 32
-            return(sub(output, 32), and(add(ol, 95),
-                0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0
-            ))
-        }
-    }
+            // Shuffle odd nibbles to consecutive bytes
+            a := and(div(mul(a, 0x11), 0x10),
+0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF
+            )
+            a := and(div(mul(a, 0x101), 0x100),
+0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF
+            )
+            a := and(div(mul(a, 0x10001), 0x10000),
+0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF
+            )
+            a := and(div(mul(a, 0x100000001), 0x100000000),
+0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF
+            )
+            a := mul(a, 0x100000000000000010000000000000000)
+            
+            // Store
+            mstore(o, a)
+            o := add(o, 16)
+            
+            // Loop
+            i := add(i, 32)
+        } 
+        
+        let ol := div(calldataload(36), 2)
+        mstore(0, 32)
+        mstore(32, ol)
+        
+        // Add 64 to ol and round to the next multiple of 32
+        return(0, and(add(ol, 95),
+            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0
+        ))
+    }}
 }
