1d86d631b24309ab26f771b85d5c99c940a6bd7d
Remco Bloemen <remco@wicked.ventures>
2018-05-31 16:39:25 +0200

    Unique 720103: Inline

--- contracts/Unique.sol
+++ contracts/Unique.sol
@@ -13,56 +13,34 @@ contract Unique {
     
     uint256 constant prime = 0x3cb610f2f269903047b2e6af9f5940b3ae7a667c7a5bc30f0e02a1b323a7fee1;
     
-    function hash(uint256 val) 
-        internal pure
-        returns (uint256)
-    {
-        return (val ^ flag) * prime;
-    }
-
-    function contains(uint256[] memory ht, uint256 value)
-        internal pure
-        returns (bool)
-    {
-        uint256 h = hash(value);
-        uint256 index = h % ht.length;
-        while (ht[index] != 0) {
-            if (ht[index] == h) {
-                return true;
-            }
-            index++;
-            index %= ht.length;
-        }
-        return false;
-    }
-    
-    function insert(uint256[] memory ht, uint256 value)
-        internal pure
-    {
-        uint256 h = hash(value);
-        uint256 index = h % ht.length;
-        while (ht[index] != 0) {
-            index++;
-            index %= ht.length;
-        }
-        ht[index] = h;
-    }
-    
     function uniquify(uint[] input)
         public pure
         returns(uint[])
     {
-        uint256[] memory ht = new uint256[](input.length);
+        uint256[] memory ht = new uint256[](2 * input.length);
         
         uint ptr = 0;
         for(uint i = 0; i < input.length; i++) {
             
             uint256 value = input[i];
-            if (contains(ht, value)) {
+            uint256 h = (value ^ flag) * prime;
+            uint256 index = h % ht.length;
+            
+            bool found = false;
+            while (ht[index] != 0) {
+                if (ht[index] == h) {
+                    found = true;
+                    break;
+                }
+                index++;
+                index %= ht.length;
+            }
+            if (found) {
                 continue;
             }
-            insert(ht, value);
             
+            // Insert
+            ht[index] = h;
             input[ptr] = value;
             ptr++;
         }
