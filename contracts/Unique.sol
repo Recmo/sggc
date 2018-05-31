65b246111fb4fca2babab7c05c083d36a62a35a2
Remco Bloemen <remco@wicked.ventures>
2018-05-31 16:39:25 +0200

    Unique 936628: linear probing

--- contracts/Unique.sol
+++ contracts/Unique.sol
@@ -9,29 +9,62 @@ pragma solidity 0.4.24;
 
 contract Unique {
     
+    uint256 constant flag = 0x1e2f1f54a8ec52f1f611639f57a9e0157e21f98ff738f0ea8088d26157b00f1c;
+    
+    uint256 constant prime = 0x3cb610f2f269903047b2e6af9f5940b3ae7a667c7a5bc30f0e02a1b323a7fee1;
+    
+    function hash(uint256 val) 
+        internal pure
+        returns (uint256)
+    {
+        return (val ^ flag) * prime;
+    }
+
+    function contains(uint256[] memory ht, uint256 value)
+        internal pure
+        returns (bool)
+    {
+        uint256 h = hash(value);
+        uint256 index = h % ht.length;
+        while (ht[index] != 0) {
+            if (ht[index] == h) {
+                return true;
+            }
+            index++;
+            index %= ht.length;
+        }
+        return false;
+    }
+    
+    function insert(uint256[] memory ht, uint256 value)
+        internal pure
+    {
+        uint256 h = hash(value);
+        uint256 index = h % ht.length;
+        while (ht[index] != 0) {
+            index++;
+            index %= ht.length;
+        }
+        ht[index] = h;
+    }
+    
     function uniquify(uint[] input)
         public pure
         returns(uint[])
     {
+        uint256[] memory ht = new uint256[](input.length);
+        
         uint ptr = 0;
         for(uint i = 0; i < input.length; i++) {
             
             uint256 value = input[i];
-            
-            // Check if seen before
-            bool unique = true;
-            for(uint j = 0; j < ptr; j++) {
-                if(input[j] == value) {
-                    unique = false;
-                    break;
-                }
+            if (contains(ht, value)) {
+                continue;
             }
+            insert(ht, value);
             
-            // Add to start of list
-            if (unique) {
-                input[ptr] = value;
-                ptr++;
-            }
+            input[ptr] = value;
+            ptr++;
         }
 
         // In-place return
