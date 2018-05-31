170d18e3dbff1285b8774501095ded49c1e85708
Remco Bloemen <remco@wicked.ventures>
2018-05-31 16:39:25 +0200

    Sort 619072: Use upper bits of hash

--- contracts/Unique.sol
+++ contracts/Unique.sol
@@ -9,27 +9,29 @@ pragma solidity 0.4.24;
 
 contract Unique {
     
-    uint256 constant flag = 0x1e2f1f54a8ec52f1f611639f57a9e0157e21f98ff738f0ea8088d26157b00f1c;
-    
     uint256 constant prime = 0x3cb610f2f269903047b2e6af9f5940b3ae7a667c7a5bc30f0e02a1b323a7fee1;
     
     function uniquify(uint[] input)
         public view
         returns(uint[])
     {
-        uint256 htl = 2 * input.length;
+        uint256 htl = 1 + 2 * input.length;
+        uint256 scale = 1 + uint256(-htl) / htl;
         uint256[] memory ht = new uint256[](htl);
         
         uint ptr = 0;
         for(uint i = 0; i < input.length; i++) {
             
             uint256 value = input[i];
-            uint256 h = (value ^ flag) * prime;
-            uint256 index = h % ht.length;
+            uint256 h = (value ^ prime) * prime;
+            uint256 index = h / scale;
             uint256 iv;
             
             bool found = false;
             assembly {
+                
+                
+                
                 iv := mload(add(mul(index, 32), add(32, ht)))
                 
                 jumpi(iend, iszero(iv))
