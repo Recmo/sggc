b1654bf360959ab2258e1fa5979034e281a18775
Remco Bloemen <remco@wicked.ventures>
2018-06-02 22:14:32 +0200

    Sort 549455: full asm

--- contracts/Sort.sol
+++ contracts/Sort.sol
@@ -2,80 +2,84 @@ pragma solidity ^0.4.23;
 
 contract Sort {
     
-    // @author Remco Bloemen <remco@wicked.ventures>
-    
-    function () external payable
-    {
-        assembly {
-            mstore(0x40, 0)
-        }
-        uint256 lo;
-        uint256 hi;
-        assembly {
-            calldatacopy(0, 4, calldatasize)
-            lo := 0x40
-            hi := add(lo, sub(calldatasize, 0x64))
-        }
-        sort(lo, hi);
-        assembly {
-            return(0, sub(calldatasize, 4))
-        }
-    }
-    
-    function sort(uint256 lo, uint256 hi)
-        internal view
-    {
-        if (hi - lo == 32) {
-            assembly {
-                let a := mload(lo)
-                let b := mload(hi)
-                if lt(b, a) {
-                    mstore(lo, b)
-                    mstore(hi, a)
+    function () external payable { assembly {
+        
+        // @author Remco Bloemen <remco@wicked.ventures>
+        
+        // Copy input to memory
+        mstore(0x40, 0) // Clear memory (get's rid of Solidities free ptr)
+        calldatacopy(0, 4, calldatasize)
+        
+        // Special case for zero or one value
+        jumpi(done, lt(calldatasize, 0x84))
+        
+        // Recursively sort
+        sort(0x40, sub(calldatasize, 0x24))
+        function sort(lo, hi) {
+            
+            let lolo := lo
+            let hihi := hi
+            
+            if eq(sub(hi, lo), 32) {
+                {
+                    let a := mload(lo)
+                    let b := mload(hi)
+                    if lt(b, a) {
+                        mstore(lo, b)
+                        mstore(hi, a)
+                    }
                 }
+                jump(ret)
             }
-            return;
-        }
-        
-        // Partition
-        uint256 lolo = lo;
-        uint256 hihi = hi;
-        assembly {
-            let pivot
-            let lov
-            let hiv
-            let i
-            let j
             
-            // Compute pivot value
-            pivot := and(div(add(lo, hi), 2), not(0x1F))
-            pivot := mload(pivot)
-            
-        loop:
-            lov := mload(lo)
-            hiv := mload(hi)
-            for {} lt(lov, pivot) {} {
-                lo := add(lo, 32)
+            // Partition
+            {
+                let pivot
+                let lov
+                let hiv
+                let i
+                let j
+                
+                // Compute pivot value
+                pivot := and(div(add(lo, hi), 2), not(0x1F))
+                pivot := mload(pivot)
+                
+            loop:
                 lov := mload(lo)
-            }
-            for {} gt(hiv, pivot) {} {
-                hi := sub(hi, 32)
                 hiv := mload(hi)
+                for {} lt(lov, pivot) {} {
+                    lo := add(lo, 32)
+                    lov := mload(lo)
+                }
+                for {} gt(hiv, pivot) {} {
+                    hi := sub(hi, 32)
+                    hiv := mload(hi)
+                }
+                jumpi(end, iszero(lt(lo, hi)))
+                mstore(lo, hiv)
+                mstore(hi, lov)
+                lo := add(lo, 32)
+                hi := sub(hi, 32)
+                jump(loop)
+                
+            end:
+                lo := hi
+                hi := add(lo, 32)
             }
-            jumpi(end, iszero(lt(lo, hi)))
-            mstore(lo, hiv)
-            mstore(hi, lov)
-            lo := add(lo, 32)
-            hi := sub(hi, 32)
-            jump(loop)
             
-        end:
-            lo := hi
-            hi := add(lo, 32)
+            // Recurse
+            if lt(lolo, lo) {
+                sort(lolo, lo)
+            }
+            if lt(hi, hihi) {
+                sort(hi, hihi)
+            }
+            
+        ret:
         }
         
-        // Recurse
-        if (lolo < lo) sort(lolo, lo);
-        if (hi < hihi) sort(hi, hihi);
-    }
+        
+    done:
+        return(0, sub(calldatasize, 4))
+    }}
 }
