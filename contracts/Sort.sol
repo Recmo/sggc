3e7a7e07652e70b0026ab4a7a0c43fe97279e00b
Remco Bloemen <remco@wicked.ventures>
2018-06-02 19:22:50 +0200

    Sort 1385774: inline

--- contracts/Sort.sol
+++ contracts/Sort.sol
@@ -8,63 +8,6 @@ contract Sort {
         heapSort(input, input.length);
         return input;
     }
-
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
-    function heapify(uint[] arr, uint n, uint i)
-        internal view
-    {
-        assembly {
-            
-            let top
-            let iaddr // Root
-            let ival
-            let maddr // Maximum
-            let mval
-            let taddr // Temporary
-            let tval
-            
-            arr := add(arr, 32)
-            top := add(mul(n, 32), arr)
-            iaddr := add(mul(i, 32), arr)
-            ival := mload(iaddr)
-            
-        loop:
-            
-            // Start with the root as largest
-            maddr := iaddr
-            mval := ival
-            
-            // If left child is larger than root
-            taddr := add(arr, mul(sub(iaddr, arr), 2))
-            tval := mload(taddr)
-            if and(lt(taddr, top), gt(tval, mval)) {
-                maddr := taddr
-                mval := tval
-            }
-            
-            // If right child is larger than largest so far
-            taddr := add(taddr, 32)
-            tval := mload(taddr)
-            if and(lt(taddr, top), gt(tval, mval)) {
-                maddr := taddr
-                mval := tval
-            }
-            
-            // If root is largest we are done
-            jumpi(end, eq(maddr, iaddr))
-            
-            // Swap largest with root
-            mstore(iaddr, mval)
-            mstore(maddr, ival)
-            
-            // Repeat for subtree starting at largest
-            iaddr := maddr
-            jump(loop)
-            
-        end:
-        }
-    }
     
     function heapSort(uint[] arr, uint n)
         internal view
@@ -72,8 +15,57 @@ contract Sort {
         int i;
         
         // Build heap (rearrange array)
-        for (i = int(n) / 2 - 1; i >= 0; i--)
-            heapify(arr, n, uint(i));
+        for (i = int(n) / 2 - 1; i >= 0; i--) {
+            assembly {
+                
+                let base
+                let iaddr // Root
+                let ival
+                let maddr // Maximum
+                let mval
+                let taddr // Temporary
+                let tval
+                
+                base := add(arr, 32)
+                iaddr := add(mul(i, 32), base)
+                ival := mload(iaddr)
+                
+            loop:
+                
+                // Start with the root as largest
+                maddr := iaddr
+                mval := ival
+                
+                // If left child is larger than root
+                taddr := add(base, mul(sub(iaddr, base), 2))
+                tval := mload(taddr)
+                if gt(tval, mval) {
+                    maddr := taddr
+                    mval := tval
+                }
+                
+                // If right child is larger than largest so far
+                taddr := add(taddr, 32)
+                tval := mload(taddr)
+                if gt(tval, mval) {
+                    maddr := taddr
+                    mval := tval
+                }
+                
+                // If root is largest we are done
+                jumpi(end, eq(maddr, iaddr))
+                
+                // Swap largest with root
+                mstore(iaddr, mval)
+                mstore(maddr, ival)
+                
+                // Repeat for subtree starting at largest
+                iaddr := maddr
+                jump(loop)
+                
+            end:
+            }
+        }
         
         // One by one extract an element from heap
         for (i = int(n) - 1; i >= 0; i--)
@@ -82,7 +74,57 @@ contract Sort {
             (arr[0], arr[uint(i)]) = (arr[uint(i)], arr[0]);
      
             // call max heapify on the reduced heap
-            heapify(arr, uint(i), 0);
+            assembly {
+                
+                let base
+                let top
+                let iaddr // Root
+                let ival
+                let maddr // Maximum
+                let mval
+                let taddr // Temporary
+                let tval
+                
+                base := add(arr, 32)
+                top := add(mul(i, 32), base)
+                iaddr := base
+                ival := mload(iaddr)
+                
+            loop:
+                
+                // Start with the root as largest
+                maddr := iaddr
+                mval := ival
+                
+                // If left child is larger than root
+                taddr := add(base, mul(sub(iaddr, base), 2))
+                tval := mload(taddr)
+                if and(lt(taddr, top), gt(tval, mval)) {
+                    maddr := taddr
+                    mval := tval
+                }
+                
+                // If right child is larger than largest so far
+                taddr := add(taddr, 32)
+                tval := mload(taddr)
+                if and(lt(taddr, top), gt(tval, mval)) {
+                    maddr := taddr
+                    mval := tval
+                }
+                
+                // If root is largest we are done
+                jumpi(end, eq(maddr, iaddr))
+                
+                // Swap largest with root
+                mstore(iaddr, mval)
+                mstore(maddr, ival)
+                
+                // Repeat for subtree starting at largest
+                iaddr := maddr
+                jump(loop)
+                
+            end:
+            }
         }
     }
 }
