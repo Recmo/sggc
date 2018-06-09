f69b210bdb2cb43a3826a6f474050b528b61215b
Remco Bloemen <remco@wicked.ventures>
2018-06-09 13:09:37 +0200

    Brain: add author

--- contracts/BrainFuck.sol
+++ contracts/BrainFuck.sol
@@ -1,13 +1,8 @@
-/**
- * This file is part of the 1st Solidity Gas Golfing Contest.
- *
- * This work is licensed under Creative Commons Attribution ShareAlike 3.0.
- * https://creativecommons.org/licenses/by-sa/3.0/
- */
-
 pragma solidity ^0.4.23;
 
 contract BrainFuck {
+    
+    /// @author Remco Bloemen <remco@wicked.ventures>
 
     function execute(bytes pro, bytes inp)
         public pure
