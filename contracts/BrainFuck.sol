0042dd0987785b10e7d3dd3d67ae00ac838caa7b
Remco Bloemen <remco@wicked.ventures>
2018-06-09 12:27:33 +0200

    Brain 4836329: rename vars

--- contracts/BrainFuck.sol
+++ contracts/BrainFuck.sol
@@ -8,62 +8,53 @@
 pragma solidity ^0.4.23;
 
 contract BrainFuck {
-    /**
-     * @dev Executes a BrainFuck program, as described at https://en.wikipedia.org/wiki/Brainfuck.
-     *
-     * Memory cells, input, and output values are all expected to be 8 bit
-     * integers. Incrementing past 255 should overflow to 0, and decrementing
-     * below 0 should overflow to 255.
-     *
-     * Programs and input streams may be of any length. The memory tape starts
-     * at cell 0, and will never be moved below 0 or above 1023. No program will
-     * output more than 1024 values.
-     *
-     * @param program The BrainFuck program.
-     * @param input The program's input stream.
-     * @return The program's output stream. Should be exactly the length of the
-     *          number of outputs produced by the program.
-     */
-    function execute(bytes program, bytes input) public pure returns(bytes) {
-        uint256 pl = program.length;
+
+    function execute(bytes pro, bytes inp)
+        public pure
+        returns(bytes)
+    {
+        uint256 pl = pro.length;
         
-        uint ipp = 0;
-        uint opp = 0;
-        uint dp = 0;
+        uint256 pp = 0;
+        uint256 ip = 0;
+        uint256 op = 0;
+        uint256 mp = 0;
+        
         bytes memory mem = new bytes(1024);
-        bytes memory output = new bytes(1024);
+        bytes memory out = new bytes(1024);
         uint256[] memory arg = new uint256[](pl);
-        compile(program, arg);
         
-        for(uint ip = 0; ip < pl; ip++) {
-            bytes1 instruction = program[ip];
+        compile(pro, arg);
+        
+        for(pp = 0; pp < pl; pp++) {
+            bytes1 instruction = pro[pp];
             if(instruction == '+') {
-                mem[dp] = byte(uint(mem[dp]) + 1);
+                mem[mp] = byte(uint(mem[mp]) + 1);
             } else if(instruction == '-') {
-                mem[dp] = byte(uint(mem[dp]) - 1);
+                mem[mp] = byte(uint(mem[mp]) - 1);
             } else if(instruction == '>') {
-                dp++;
+                mp++;
             } else if(instruction == '<') {
-                dp--;
+                mp--;
             } else if(instruction == '.') {
-                output[opp++] = mem[dp];
+                out[op++] = mem[mp];
             } else if(instruction == ',') {
-                mem[dp] = input[ipp++];
+                mem[mp] = inp[ip++];
             } else if(instruction == '[') {
-                if(mem[dp] == 0) {
-                    ip = arg[ip];
+                if(mem[mp] == 0) {
+                    pp = arg[pp];
                 }
             } else if(instruction == ']') {
-                if(mem[dp] != 0) {
-                    ip = arg[ip];
+                if(mem[mp] != 0) {
+                    pp = arg[pp];
                 }
             }
         }
-
+        
         // Create output array
-        bytes memory ret = new bytes(opp);
-        for(uint256 i = 0; i < opp; i++) {
-            ret[i] = output[i];
+        bytes memory ret = new bytes(op);
+        for(uint256 i = 0; i < op; i++) {
+            ret[i] = out[i];
         }
         return ret;
     }
