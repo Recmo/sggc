c4b32747523727318d236864d589b059a612af44
Remco Bloemen <remco@wicked.ventures>
2018-06-09 12:43:29 +0200

    Brain 5153830: instafuncs

--- contracts/BrainFuck.sol
+++ contracts/BrainFuck.sol
@@ -57,59 +57,134 @@ contract BrainFuck {
         for(; pp < pro.length;) {
             bytes1 instruction = pro[pp];
             if(instruction == '+') {
-                mem[mp] = byte(uint(mem[mp]) + 1);
-                pp++;
+                (mp, ip, op, pp) = incr(mem, mp, inp, ip, out, op, arg, pp);
             } else if(instruction == '-') {
-                mem[mp] = byte(uint(mem[mp]) - 1);
-                pp++;
+                (mp, ip, op, pp) = decr(mem, mp, inp, ip, out, op, arg, pp);
             } else if(instruction == '>') {
-                mp++;
-                pp++;
+                (mp, ip, op, pp) = right(mem, mp, inp, ip, out, op, arg, pp);
             } else if(instruction == '<') {
-                mp--;
-                pp++;
+                (mp, ip, op, pp) = left(mem, mp, inp, ip, out, op, arg, pp);
             } else if(instruction == '.') {
-                out[op++] = mem[mp];
-                pp++;
+                (mp, ip, op, pp) = output(mem, mp, inp, ip, out, op, arg, pp);
             } else if(instruction == ',') {
-                mem[mp] = inp[ip++];
-                pp++;
+                (mp, ip, op, pp) = input(mem, mp, inp, ip, out, op, arg, pp);
             } else if(instruction == '[') {
-                if(mem[mp] == 0) {
-                    pp = arg[pp];
-                } else {
-                    pp++;
-                }
+                (mp, ip, op, pp) = open(mem, mp, inp, ip, out, op, arg, pp);
             } else if(instruction == ']') {
-                if(mem[mp] != 0) {
-                    pp = arg[pp];
-                } else {
-                    pp++;
-                }
+                (mp, ip, op, pp) = close(mem, mp, inp, ip, out, op, arg, pp);
             } else {
                 pp++;
             }
         }
     }
-        }
-        return ret;
+    
+    function right(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
+    {
+        mp++;
+        pp++;
+        return (mp, ip, op, pp);
     }
     
-    function compile(bytes memory pro, uint256[] memory arg)
-        private pure
+    function left(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
     {
-        // Compute arguments
-        uint256[20] memory stack;
-        uint256 sp = 0;
-        for(uint pp = 0; pp < pro.length; pp++) {
-            bytes1 instruction = pro[pp];
-            if(instruction == '[') {
-                stack[sp++] = pp;
-            } else if(instruction == ']') {
-                uint256 matchp = stack[--sp];
-                arg[matchp] = pp;
-                arg[pp] = matchp;
-            }
+        mp--;
+        pp++;
+        return (mp, ip, op, pp);
+    }
+    
+    function incr(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
+    {
+        mem[mp] = bytes1(uint8(mem[mp]) + 1);
+        pp++;
+        return (mp, ip, op, pp);
+    }
+    
+    function decr(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
+    {
+        mem[mp] = bytes1(uint8(mem[mp]) - 1);
+        pp++;
+        return (mp, ip, op, pp);
+    }
+    
+    function output(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
+    {
+        out[op++] = mem[mp];
+        pp++;
+        return (mp, ip, op, pp);
+    }
+    
+    function input(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
+    {
+        mem[mp] = inp[ip++];
+        pp++;
+        return (mp, ip, op, pp);
+    }
+    
+    function open(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
+    {
+        if (mem[mp] == 0) {
+            pp = arg[pp];
+        } else {
+            pp++;
         }
+        return (mp, ip, op, pp);
+    }
+    
+    function close(
+        bytes memory mem, uint256 mp,
+        bytes memory inp, uint256 ip,
+        bytes memory out, uint256 op,
+        uint256[] memory arg,
+        uint256 pp
+    ) private pure returns (uint256, uint256, uint256, uint256)
+    {
+        if (mem[mp] != 0) {
+            pp = arg[pp];
+        } else {
+            pp++;
+        }
+        return (mp, ip, op, pp);
     }
 }
