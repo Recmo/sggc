770c35131a7048e0fd1015e7b671e86b3c21e514
Remco Bloemen <remco@wicked.ventures>
2018-06-12 17:15:08 +0200

    Sort 388399: initial radix sort

--- contracts/Sort.sol
+++ contracts/Sort.sol
@@ -2,18 +2,69 @@ pragma solidity ^0.4.23;
 
 contract Sort {
     
-    function () external payable { assembly {
+    //function () external payable { assembly {
+    
+    function sort(uint[] input) external payable returns(uint[]) { assembly {
         
         // @author Remco Bloemen <remco@wicked.ventures>
         
-        // Copy input to memory
-        calldatacopy(0, 4, calldatasize)
+        mstore(0x40, 0) // Set all memory to zero
+        
+        let temp
+        let scale
+        let i
         
         // Special case for zero or one value
-        jumpi(done, lt(calldatasize, 0x84))
+        jumpi(trivial, lt(calldatasize, 0x84))
         
-        // Recursively sort
-        sort(0x40, sub(calldatasize, 0x24))
+        // Radix sort
+        // 80 * 32 = 2560  = size of bucket table
+        
+        // First pass: find upper bound to values and compute scaling factor
+        scale := 0
+        for {i := 0x44} lt(i, calldatasize) {i := add(i, 32)} {
+            scale := or(scale, calldataload(i))
+        }
+        scale := div(add(scale, 79), 80)
+        
+        // Second pass: count buckets (in multipes of 32)
+        for {i := 0x44} lt(i, calldatasize) {i := add(i, 32)} {
+            let addr := mul(div(calldataload(i), scale), 32)
+            mstore(addr, add(mload(addr), 32))
+        }
+        temp := 2560 // Include write offset
+        for {i := 0x00} lt(i, 2560) {i := add(i, 32)} {
+            temp := add(temp, mload(i))
+            mstore(i, temp)
+        }
+        
+        // Third pass: move to buckets
+        for {i := 0x44} lt(i, calldatasize) {i := add(i, 32)} {
+            temp := calldataload(i)
+            let addr1 := mul(div(temp, scale), 32)
+            let addr2 := sub(mload(addr1), 32)
+            mstore(addr1, addr2)
+            mstore(addr2, temp)
+        }
+        
+        // Fourth pass: sort buckets
+        temp := mload(0)
+        for {i := 0x20} lt(i, 2560) {i := add(i, 32)} {
+            let val := mload(i)
+            if lt(temp, sub(val, 32)) {
+                sort(temp, sub(val, 32))
+            }
+            temp := val
+        }
+        scale := add(sub(calldatasize, 0x44), sub(2560, 32))
+        if lt(temp, scale) {
+            sort(temp, scale)
+        }
+        
+        //scale := add(sub(calldatasize, 0x44), sub(2560, 32))
+        //calldatacopy(2560, 0x44, sub(calldatasize, 0x44))
+        //sort(2560, scale)
+        
         function sort(lo, hi) {
             
             let lolo := lo
@@ -114,6 +165,12 @@ contract Sort {
         
         
     done:
+        mstore(sub(2560, 0x40), 0x20)
+        mstore(sub(2560, 0x20), calldataload(0x24))
+        return(sub(2560, 0x40), sub(calldatasize, 4))
+        
+    trivial:
+        calldatacopy(0, 4, calldatasize)
         return(0, sub(calldatasize, 4))
     }}
 }
