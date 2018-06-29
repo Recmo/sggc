f0a1077f0a2cd9a1c5a541177a383ae1d4f1789c
Remco Bloemen <remco@wicked.ventures>
2018-06-29 13:38:26 +0200

    Sort: add sorting networks

--- contracts/Sort.sol
+++ contracts/Sort.sol
@@ -269,6 +269,171 @@ contract Sort {
         mstore(sub(3840, 0x20), calldataload(0x24))
         return(sub(3840, 0x40), sub(calldatasize, 4))
         
+        
+    sort2: {
+            let a := mload(addr2)
+            let b := mload(add(addr2, 32))
+            jumpi(end, gt(b, a))
+            mstore(addr2, b)
+            mstore(add(addr2, 32), a)
+        end:
+        }
+        jump(l5)
+    
+    sort3: {
+            let a := mload(addr2)
+            let b := mload(add(addr2, 32))
+            let c := mload(add(addr2, 64))
+            jumpi(case1, gt(b, a))
+            a b =: a =: b
+        case1:
+            jumpi(case3, gt(c, b))
+            b c =: b =: c
+            jumpi(case3, gt(b, a))
+            a b =: a =: b
+        case3:
+            mstore(addr2, a)
+            mstore(add(addr2, 32), b)
+            mstore(add(addr2, 64), c)
+        }
+        jump(l5)
+    
+    sort4: // [[1 2][3 4][1 3][2 4][2 3]]
+        addr2 96 add mload
+        addr2 64 add mload
+        addr2 32 add mload
+        addr2 mload
+        dup1 dup3 lt skip_4_1 jumpi swap1 skip_4_1:
+        dup3 dup5 lt skip_4_2 jumpi swap2 swap3 swap2 skip_4_2:
+        dup1 dup4 lt skip_4_3 jumpi swap2 skip_4_3:
+        dup2 dup5 lt skip_4_4 jumpi swap1 swap3 swap1 skip_4_4:
+        dup2 dup4 lt skip_4_5 jumpi swap1 swap2 swap1 skip_4_5:
+        addr2 96 add mstore
+        addr2 64 add mstore
+        addr2 32 add mstore
+        addr2 mstore
+        jump(l5)
+
+    sort5: // [[1 2][3 4][1 3][2 5][1 2][3 4][2 3][4 5][3 4]]
+        addr2 128 add mload
+        addr2 96 add mload
+        addr2 64 add mload
+        addr2 32 add mload
+        addr2 mload
+        dup1 dup3 lt skip_5_1 jumpi swap1 skip_5_1:
+        dup3 dup5 lt skip_5_2 jumpi swap2 swap3 swap2 skip_5_2:
+        dup1 dup4 lt skip_5_3 jumpi swap2 skip_5_3:
+        dup2 dup6 lt skip_5_4 jumpi swap1 swap4 swap1 skip_5_4:
+        dup1 dup3 lt skip_5_5 jumpi swap1 skip_5_5:
+        dup3 dup5 lt skip_5_6 jumpi swap2 swap3 swap2 skip_5_6:
+        dup2 dup4 lt skip_5_7 jumpi swap1 swap2 swap1 skip_5_7:
+        dup4 dup6 lt skip_5_8 jumpi swap3 swap4 swap3 skip_5_8:
+        dup3 dup5 lt skip_5_9 jumpi swap2 swap3 swap2 skip_5_9:
+        addr2 128 add mstore
+        addr2 96 add mstore
+        addr2 64 add mstore
+        addr2 32 add mstore
+        addr2 mstore
+        jump(l5)
+        
+    sort6: // [[1 2][3 4][5 6][1 3][2 5][4 6][1 2][3 4][5 6][2 3][4 5][3 4]]
+        addr2 160 add mload
+        addr2 128 add mload
+        addr2 96 add mload
+        addr2 64 add mload
+        addr2 32 add mload
+        addr2 mload
+        dup1 dup3 lt skip_6_1 jumpi swap1 skip_6_1:
+        dup3 dup5 lt skip_6_2 jumpi swap2 swap3 swap2 skip_6_2:
+        dup5 dup7 lt skip_6_3 jumpi swap4 swap5 swap4 skip_6_3:
+        dup1 dup4 lt skip_6_4 jumpi swap2 skip_6_4:
+        dup2 dup6 lt skip_6_5 jumpi swap1 swap4 swap1 skip_6_5:
+        dup4 dup7 lt skip_6_6 jumpi swap3 swap5 swap3 skip_6_6:
+        dup1 dup3 lt skip_6_7 jumpi swap1 skip_6_7:
+        dup3 dup5 lt skip_6_8 jumpi swap2 swap3 swap2 skip_6_8:
+        dup5 dup7 lt skip_6_9 jumpi swap4 swap5 swap4 skip_6_9:
+        dup2 dup4 lt skip_6_10 jumpi swap1 swap2 swap1 skip_6_10:
+        dup4 dup6 lt skip_6_11 jumpi swap3 swap4 swap3 skip_6_11:
+        dup3 dup5 lt skip_6_12 jumpi swap2 swap3 swap2 skip_6_12:
+        addr2 160 add mstore
+        addr2 128 add mstore
+        addr2 96 add mstore
+        addr2 64 add mstore
+        addr2 32 add mstore
+        addr2 mstore
+        jump(l5)
+        
+    sort7: // [[1 2][0 2][0 1][3 4][5 6][3 5][4 6][4 5][0 4][0 3][1 5][2 6][2 5][1 3][2 4][2 3]]
+        addr2 192 add mload
+        addr2 160 add mload
+        addr2 128 add mload
+        addr2 96 add mload
+        addr2 64 add mload
+        addr2 32 add mload
+        addr2 mload
+        dup2 dup4 lt skip_7_1 jumpi swap1 swap2 swap1 skip_7_1:
+        dup1 dup4 lt skip_7_2 jumpi swap2 skip_7_2:
+        dup1 dup3 lt skip_7_3 jumpi swap1 skip_7_3:
+        dup4 dup6 lt skip_7_4 jumpi swap3 swap4 swap3 skip_7_4:
+        dup6 dup8 lt skip_7_5 jumpi swap5 swap6 swap5 skip_7_5:
+        dup4 dup7 lt skip_7_6 jumpi swap3 swap5 swap3 skip_7_6:
+        dup5 dup8 lt skip_7_7 jumpi swap4 swap6 swap4 skip_7_7:
+        dup5 dup7 lt skip_7_8 jumpi swap4 swap5 swap4 skip_7_8:
+        dup1 dup6 lt skip_7_9 jumpi swap4 skip_7_9:
+        dup1 dup5 lt skip_7_10 jumpi swap3 skip_7_10:
+        dup2 dup7 lt skip_7_11 jumpi swap1 swap5 swap1 skip_7_11:
+        dup3 dup8 lt skip_7_12 jumpi swap2 swap6 swap2 skip_7_12:
+        dup3 dup7 lt skip_7_13 jumpi swap2 swap5 swap2 skip_7_13:
+        dup2 dup5 lt skip_7_14 jumpi swap1 swap3 swap1 skip_7_14:
+        dup3 dup6 lt skip_7_15 jumpi swap2 swap4 swap2 skip_7_15:
+        dup3 dup5 lt skip_7_16 jumpi swap2 swap3 swap2 skip_7_16:
+        addr2 192 add mstore
+        addr2 160 add mstore
+        addr2 128 add mstore
+        addr2 96 add mstore
+        addr2 64 add mstore
+        addr2 32 add mstore
+        addr2 mstore
+        jump(l5)
+        
+    sort8:
+        addr2 224 add mload
+        addr2 192 add mload
+        addr2 160 add mload
+        addr2 128 add mload
+        addr2 96 add mload
+        addr2 64 add mload
+        addr2 32 add mload
+        addr2 mload
+        dup1 dup3 lt skip_8_1 jumpi swap1 skip_8_1:
+        dup3 dup5 lt skip_8_2 jumpi swap2 swap3 swap2 skip_8_2:
+        dup1 dup4 lt skip_8_3 jumpi swap2 skip_8_3:
+        dup2 dup5 lt skip_8_4 jumpi swap1 swap3 swap1 skip_8_4:
+        dup2 dup4 lt skip_8_5 jumpi swap1 swap2 swap1 skip_8_5:
+        dup5 dup7 lt skip_8_6 jumpi swap4 swap5 swap4 skip_8_6:
+        dup7 dup9 lt skip_8_7 jumpi swap6 swap7 swap6 skip_8_7:
+        dup5 dup8 lt skip_8_8 jumpi swap4 swap6 swap4 skip_8_8:
+        dup6 dup9 lt skip_8_9 jumpi swap5 swap7 swap5 skip_8_9:
+        dup6 dup8 lt skip_8_10 jumpi swap5 swap6 swap5 skip_8_10:
+        dup1 dup6 lt skip_8_11 jumpi swap4 skip_8_11:
+        dup2 dup7 lt skip_8_12 jumpi swap1 swap5 swap1 skip_8_12:
+        dup2 dup6 lt skip_8_13 jumpi swap1 swap4 swap1 skip_8_13:
+        dup3 dup8 lt skip_8_14 jumpi swap2 swap6 swap2 skip_8_14:
+        dup4 dup9 lt skip_8_15 jumpi swap3 swap7 swap3 skip_8_15:
+        dup4 dup8 lt skip_8_16 jumpi swap3 swap6 swap3 skip_8_16:
+        dup3 dup6 lt skip_8_17 jumpi swap2 swap4 swap2 skip_8_17:
+        dup4 dup7 lt skip_8_18 jumpi swap3 swap5 swap3 skip_8_18:
+        dup4 dup6 lt skip_8_19 jumpi swap3 swap4 swap3 skip_8_19:
+        addr2 224 add mstore
+        addr2 192 add mstore
+        addr2 160 add mstore
+        addr2 128 add mstore
+        addr2 96 add mstore
+        addr2 64 add mstore
+        addr2 32 add mstore
+        addr2 mstore
+        jump(l5)
+                
     trivial:
         calldatacopy(0, 4, calldatasize)
         return(0, sub(calldatasize, 4))
