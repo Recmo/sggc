555598c2d18ea8e370291bf18c2d399a5758f1f6
Remco Bloemen <remco@wicked.ventures>
2018-06-29 17:17:05 +0200

    Brain: add update.sh

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+solc ./contracts/BrainFuck.sol 2>&1 | grep Error
+set -e
+solc --optimize --optimize-runs 200 --asm ./contracts/BrainFuck.sol > ./contracts/BrainFuck.asm 2> /dev/null
+yarn test test/BrainFuck.js  2> /dev/null
